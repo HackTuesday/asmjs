4d739787b6ae01b5ae89abce7889b60ea6e7266c
Ryan Westphal <ryan@trippingthebits.com>
2013-04-23 20:51:06 -0400

    fast fib in firefox!

--- rasmjs/rasmjs.js
+++ rasmjs/rasmjs.js
@@ -1,4 +1,12 @@
 function rasmjs() {
 
+  var myModule = rasmod( window );
 
+  var start = ( new Date() ).getTime();
+
+  var fib = myModule.fibonacci( 40 );
+  document.getElementById("output").value = fib;
+
+  var end = ( new Date() ).getTime();
+  document.getElementById("time").value = end - start;
 }
